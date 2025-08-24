7cf02d07ccba6ce8ad5af489652b95a393a3a353
SomeAB <ssabs@protonmail.com>
2025-08-24 23:18:29 +0530

    #09 Changes Notes: - Just trying out making a python port of the franc standalone, so started the file - Did some tests, but those files are just temporary so ignored

---
+++ franc_all_standalone.py
@@ -0,0 +1,17 @@
+""""
+Franc All Standalone - Python Port by SomeAB
+Version: 0.01
+
+This is a single file standalone implementation of the Franc language detection library. This is based on the 'all' version that contains the maximum number of supported languages.
+
+"""
+
+# Core Python Imports
+import re # for regular expressions
+from typing import Dict, List, Tuple, Optional, Union # for type hinting
+from collections import defaultdict # for default dictionary
+
+def normalize_whitespace(text: str, options: dict = None) -> str:
+    """Direct port of JavaScript function d(i, a)"""
+    return
+
