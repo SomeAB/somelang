f32fff1e4e98920e0444321dd13eb85b61a06951
SomeAB <ssabs@protonmail.com>
2025-09-02 01:30:06 +0530

    #27 Changes New: - Added the complex mapping for Chinese, Japanese and Korean in script_patterns
    Notes:
    - This is a minor push to backup our progress, the file is still work in progress

--- somelang/script_patterns.py
+++ somelang/script_patterns.py
@@ -23,19 +23,32 @@ _cjk_pattern = re.compile(r'[\u4E00-\u9FFF\u3400-\u4DBF\u2F00-\u2FDF\u2E80-\u2EF
 
 _arabic_pattern = re.compile(r'[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\u0870-\u089F\uFB50-\uFDFF\uFE70-\uFEFF]')
 
-_cyrillic_pattern = re.compile(r'[\u0400-\u04FF\u0500-\u052F\u2DE0-\u2DFF\uA640-\uA69F\u1C80-\u1C8F]')
-
 _devanagari_pattern = re.compile(r'[\u0900-\u097F\uA8E0-\uA8FF]')
 
+_cyrillic_pattern = re.compile(r'[\u0400-\u04FF\u0500-\u052F\u2DE0-\u2DFF\uA640-\uA69F\u1C80-\u1C8F]')
 
+_bengali_assamese_pattern = re.compile(r'[\u0980-\u09FF]')
 
+# Full Japanese
+_full_japanese_pattern = re.compile(r'[\u4E00-\u9FFF\u3400-\u4DBF\u2F00-\u2FDF\u2E80-\u2EFF\u31C0-\u31EF\U00020000-\U0002A6DF\u3040-\u309F\U0001B100-\U0001B12F\U0001AFF0-\U0001AFFF\U0001B000-\U0001B0FF\U0001B130-\U0001B16F\u3190-\u319F\u30A0-\u30FF\u31F0-\u31FF\uFF00-\uFFEF]')
 
+# Full minus Kanji (from CJK) and Kanbun (Kanji with annotations, Kanbun block)
+_japanese_without_chinese_pattern = re.compile(r'[\u3040-\u309F\U0001B100-\U0001B12F\U0001AFF0-\U0001AFFF\U0001B000-\U0001B0FF\U0001B130-\U0001B16F\u30A0-\u30FF\u31F0-\u31FF\uFF00-\uFFEF]')
 
+# Hiragana only
+_hiragana_pattern = re.compile(r'[\u3040-\u309F]')
 
+# Katakana plus Kana
+_katakana_pattern = re.compile(r'[\U0001B100-\U0001B12F\U0001AFF0-\U0001AFFF\U0001B000-\U0001B0FF\U0001B130-\U0001B16F\u30A0-\u30FF\u31F0-\u31FF\uFF00-\uFFEF]')
 
+# Full Korean
+_korean_pattern = re.compile(r'[\u4E00-\u9FFF\u3400-\u4DBF\u2F00-\u2FDF\u2E80-\u2EFF\u31C0-\u31EF\U00020000-\U0002A6DF\u1100-\u11FF\uA960-\uA97F\uD7B0-\uD7FF\u3130-\u318F\uFF00-\uFFEF\uAC00-\uD7AF]')
+
+# Hangul only (Korean without Hanja from CJK block)
+_hangul_pattern = re.compile(r'[\u1100-\u11FF\uA960-\uA97F\uD7B0-\uD7FF\u3130-\u318F\uFF00-\uFFEF\uAC00-\uD7AF]')
 
 # Next we map script names (both code & verbose name per ISO 15924) to the regex patterns
-# In order of most widely used script
+# In order of most widely used script first
 # We use Mapping from typing module for type hinting
 # We use MappingProxyType for mutability protection
 ALL_SCRIPT_PATTERNS: Mapping[str, re.Pattern] = MappingProxyType({
@@ -43,22 +56,41 @@ ALL_SCRIPT_PATTERNS: Mapping[str, re.Pattern] = MappingProxyType({
     'Latn': _latin_pattern,
     'Latin': _latin_pattern,
 
-    'Hant' : _cjk_pattern,
-    'Traditional' : _cjk_pattern,
+    'Hant': _cjk_pattern,
+    'Traditional': _cjk_pattern,
 
-    'Hans' : _cjk_pattern,
-    'Simplified' : _cjk_pattern,
+    'Hans': _cjk_pattern,
+    'Simplified': _cjk_pattern,
 
-    'Arab' : _arabic_pattern,
-    'Arabic' : _arabic_pattern,
+    'Arab': _arabic_pattern,
+    'Arabic': _arabic_pattern,
 
+    'Deva': _devanagari_pattern,
+    'Devanagari': _devanagari_pattern,
+    
     'Cyrl': _cyrillic_pattern,
     'Cyrillic': _cyrillic_pattern,
 
-    'Deva' : _devanagari_pattern,
-    'Devanagari' : _devanagari_pattern,
+    'Beng': _bengali_assamese_pattern,
+    'Bangla': _bengali_assamese_pattern,
 
-    
+    'Jpan': _full_japanese_pattern,
+    'Japanese': _full_japanese_pattern,
+
+    'Hrkt': _japanese_without_chinese_pattern,
+    'Japanese syllabaries': _japanese_without_chinese_pattern,
+
+    'Hira': _hiragana_pattern,
+    'Hiragana': _hiragana_pattern,
+
+    'Kana': _katakana_pattern,
+    'Katakana': _katakana_pattern,
+
+    'Kore': _korean_pattern,
+    'Korean': _korean_pattern,
+
+    'Hang': _hangul_pattern,
+    'Hangul': _hangul_pattern
 
 })
 
